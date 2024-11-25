0ae3daa17a2358536dbce0d44bb8d7fe6926a60d
Lyago Monteiro <l.monteiro@intcom.com.br>
2024-11-24 21:27:35 -0300

    Removing lgf-angular workspace

--- lgf-angular/projects/lgf-angular-furnishings/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of lgf-angular-furnishings
- */
-
-export * from './lib/lgf-angular-furnishings.service';
-export * from './lib/lgf-angular-furnishings.component';
--- lgf-angular/projects/lgf-angular-hearth/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of lgf-angular-hearth
- */
-
-export * from './lib/lgf-angular-hearth.service';
-export * from './lib/lgf-angular-hearth.component';
