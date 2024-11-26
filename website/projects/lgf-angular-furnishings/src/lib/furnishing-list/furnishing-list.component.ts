744c988ac5722492bdfdd5677d6b3e0dbe026d7d
Lyago Monteiro <l.monteiro@intcom.com.br>
2024-11-25 23:18:32 -0300

    furnishing-list with animation

--- website/projects/lgf-angular-furnishings/src/lib/furnishing-list/furnishing-list.component.ts
+++ website/projects/lgf-angular-furnishings/src/lib/furnishing-list/furnishing-list.component.ts
@@ -1,20 +1,21 @@
 import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 import { LgfFurnishingListItem } from './models/furnishing-list-item';
 import { CommonModule } from '@angular/common';
+import { LgfTriggers } from 'lgf-angular-cinema';
 
 @Component({
   selector: 'lgf-furnishing-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './furnishing-list.component.html',
-  styleUrl: './furnishing-list.component.scss'
+  styleUrl: './furnishing-list.component.scss',
+  animations: [LgfTriggers.openClosedYCollapseAndZoom('sectionCollapse', { startHeight: '0px', endHeight: '*', timings: '150ms ease-out' })]
 })
 export class FurnishingListComponent {
 
   @Input() listItems?: LgfFurnishingListItem[] = [];
   @Input() config?: { style?: string };
-  @Input() xCollapse?: boolean = true;
-  @Input() yCollapse?: boolean = false;
+  @Input() collapse?: boolean = false;
   @ContentChild('listHeadingTmp') listHeadingTmp?: TemplateRef<any>;
   @ContentChild('listItemTmp') listItemTmp?: TemplateRef<any>;
 
