6f1b5e60e2222bb414bc788fe53f95548eca93d2
Lyago Monteiro <l.monteiro@intcom.com.br>
2024-11-29 19:34:36 -0300

    Removed native animation from furnishing-list

--- website/projects/lgf-angular-furnishings/src/lib/furnishing-list/furnishing-list.component.ts
+++ website/projects/lgf-angular-furnishings/src/lib/furnishing-list/furnishing-list.component.ts
@@ -1,15 +1,13 @@
 import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 import { LgfFurnishingListItem } from './models/furnishing-list-item';
 import { CommonModule } from '@angular/common';
-import { LgfTriggers } from 'lgf-angular-cinema';
 
 @Component({
   selector: 'lgf-furnishing-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './furnishing-list.component.html',
-  styleUrl: './furnishing-list.component.scss',
-  animations: [LgfTriggers.openClosedYCollapseAndZoom('sectionCollapse', { startHeight: '0px', endHeight: '*', timings: '150ms ease-out' })]
+  styleUrl: './furnishing-list.component.scss'
 })
 export class FurnishingListComponent {
 
